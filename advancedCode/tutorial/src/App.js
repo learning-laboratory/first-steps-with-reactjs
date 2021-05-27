07bbac815ac445f55446e1bf6cdd5097c0cecacd
Mario Manuel Mabande <mario99manuel@gmail.com>
2021-05-27 09:09:19 +0200

    useRef

--- advancedCode/tutorial/src/App.js
+++ advancedCode/tutorial/src/App.js
@@ -10,13 +10,13 @@ import React from 'react'
 //import Setup from './tutorial/2-useEffect/setup/2-useEffect-cleanup'
 //import Setup from './tutorial/2-useEffect/setup/3-useEffect-fetch-data'
 
-//import Setup from './tutorial/3-conditional-rendering/setup/1-multiple-returns'
+// import Setup from './tutorial/3-conditional-rendering/setup/1-multiple-returns'
 // import Setup from './tutorial/3-conditional-rendering/setup/2-short-circuit'
 // import Setup from './tutorial/3-conditional-rendering/setup/3-show-hide'
 // import Setup from './tutorial/4-forms/setup/1-controlled-inputs'
-import Setup from './tutorial/4-forms/setup/2-multiple-inputs'
-
-
+// import Setup from './tutorial/4-forms/setup/2-multiple-inputs'
+import Setup from './tutorial/5-useRef/setup/1-useRef-basics'
+ 
 function App() {
   return (
     <div className='container'>
