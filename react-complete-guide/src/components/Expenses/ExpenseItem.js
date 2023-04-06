242c4030d6ca93a089e9f8ba557da3deb227a1e9
mariomthree <mario99manuel@gmail.com>
2023-04-06 13:13:07 +0200

    create filter component

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -4,24 +4,13 @@ import React, { useState } from 'react';
 import Card from '../UI/Card';
 
 function ExpenseItem(props) {
-
-    const [title, setTitle] = useState(props.title);
-    const [amount, setAmount] = useState(props.amount);
-    const [date, setDate] = useState(props.date);
-
-    const clickHandler = () => {
-        setTitle("Clicked");
-        console.log(title);
-    }
-
     return (
         <Card className="expense-item">
-            <ExpenseDate date={date} />
+            <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{title}</h2>
-                <div className="expense-item__price">{amount}</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">{props.amount}</div>
             </div>
-            <button onClick={clickHandler}>Edit</button>
         </Card>
     );
 }
--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,11 +1,19 @@
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 
 function Expenses({ expenses }) {
+
+    const [year, setYear] = useState('2019');
+    const onSelectYearHandler = yearSelected => {
+        setYear(yearSelected);
+    }
+
     return (
-        <Card  className="expenses">
+        <Card className="expenses">
+            <ExpensesFilter selected={year} onSelectYear={onSelectYearHandler} />
             {expenses.map((expense) => (
                 <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
             ))}
